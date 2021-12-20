4230ce5c40e004b113c07512d9c68bd234f45165
Enrique Caballero <enrcabmun@alum.us.es>
2021-12-20 18:29:50 +0100

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS adCampaigns;
 DROP TABLE IF EXISTS Reparations;
-DROP TABLE IF EXISTS Tickets;
+DROP TABLE IF EXISTS Rides;
 DROP TABLE IF EXISTS Scooters;
 DROP TABLE IF EXISTS ScooterZones;
 DROP TABLE IF EXISTS Warehouses;
@@ -11,57 +11,50 @@ DROP TABLE IF EXISTS Users;
 
 CREATE TABLE Users(
 	userId INT NOT NULL AUTO_INCREMENT,
-	username VARCHAR(60),
-	passworduser VARCHAR(60),
+	username VARCHAR(60) NOT NULL,
+	passworduser VARCHAR(60) NOT NULL,
+	dni CHAR(9) NOT NULL,
+	nameofuser VARCHAR(20),
+	surname VARCHAR(20),
 	PRIMARY KEY(userId),
-	UNIQUE (username)
+	UNIQUE (username, dni)
 );
 
 CREATE TABLE Customers(
 	customerId INT NOT NULL AUTO_INCREMENT,
-	dni CHAR(9),
 	userId INT NOT NULL,
-	balance DOUBLE,
-	nameCus VARCHAR(20),
-	surname VARCHAR(20),
-	age INT,
+	balance DOUBLE NOT NULL DEFAULT 0,
+	age INT NOT NULL,
 	paymentMethod ENUM ('oneTime', 'monthly', 'yearly'), 
 	PRIMARY KEY(customerId),
 	FOREIGN KEY(userId) REFERENCES Users(userId)
 					ON DELETE CASCADE,
-	UNIQUE (dni),
 	CONSTRAINT CustomerTooYoung CHECK (age>=18),
 	CONSTRAINT NegativeBalance CHECK (balance>=0)
 );
 
 CREATE TABLE SalesManagers(
 	salesManagerId INT NOT NULL AUTO_INCREMENT,
-	dni CHAR(9),
-	userId INT,
-	NameSal VARCHAR(20),
-	surname VARCHAR(20),
+	userId INT NOT NULL,
+	salary DOUBLE DEFAULT 900,
 	PRIMARY KEY(salesManagerId),
 	FOREIGN KEY(userId) REFERENCES Users(userId)
-					ON DELETE CASCADE,
-	UNIQUE (dni)
+					ON DELETE CASCADE
 );
 
 CREATE TABLE StockManagers(
 	stockManagerId INT NOT NULL AUTO_INCREMENT,
-	dni CHAR(9),
-	userId INT,
-	NameSto VARCHAR(20),
-	surname VARCHAR(20),
+	userId INT NOT NULL,
+	salary DOUBLE DEFAULT 900,
 	PRIMARY KEY(stockManagerId),
 	FOREIGN KEY(userId) REFERENCES Users(userId)
-					ON DELETE CASCADE,
-	UNIQUE (dni)
+					ON DELETE CASCADE
 );
 
 CREATE TABLE Warehouses(
 	warehouseId INT NOT NULL AUTO_INCREMENT,
 	stockManagerId INT,
-	location VARCHAR(60),
+	location VARCHAR(60) NOT NULL,
 	PRIMARY KEY(warehouseId),
 	FOREIGN KEY(stockManagerId) REFERENCES StockManagers (stockManagerId) 
 			ON DELETE SET NULL
@@ -70,7 +63,7 @@ CREATE TABLE Warehouses(
 CREATE TABLE ScooterZones(
 	scooterZoneId INT NOT NULL AUTO_INCREMENT,
 	stockManagerId INT,
-	location VARCHAR(60),
+	location VARCHAR(60) NOT NULL,
 	PRIMARY KEY (scooterZoneId),
 	FOREIGN KEY(stockManagerId) REFERENCES StockManagers (stockManagerId)
    		ON DELETE SET NULL
@@ -78,12 +71,12 @@ CREATE TABLE ScooterZones(
 
 CREATE TABLE Scooters(
 	scooterId INT NOT NULL AUTO_INCREMENT,
-	licensePlate CHAR(4),
+	licensePlate CHAR(4) NOT NULL,
 	scooterZoneId INT,
 	warehouseId INT,
 	scooterColor ENUM ('White','Black','Red','Blue','Yellow','Green'),
 	battery INT,
-	scooterType ENUM ('allTerrain','city','stroll'),
+	scooterType ENUM ('allTerrain','city','stroll') NOT NULL,
 	PRIMARY KEY(scooterId),
 	FOREIGN KEY(scooterZoneId) REFERENCES ScooterZones (scooterZoneId) 
 							ON DELETE SET NULL,
@@ -93,25 +86,27 @@ CREATE TABLE Scooters(
 	CONSTRAINT ScooterOnlyOnWarehouseOrZone CHECK (
     Scooters.warehouseId IS NULL
     OR Scooters.scooterZoneId IS NULL),
-   CONSTRAINT BatteryOutOfLimits CHECK(battery <= 100 AND battery >= 0)
+   CONSTRAINT BatteryOutOfLimits CHECK(battery BETWEEN 0 AND 100),
+	CONSTRAINT RentingLess20Battery CHECK (Scooters.warehouseId IS NOT NULL OR Scooters.scooterZoneId IS NOT NULL OR battery >= 20)
 
 );
 
-CREATE TABLE Tickets(
-   ticketId INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE Rides(
+	rideId INT NOT NULL AUTO_INCREMENT,
    scooterId INT,
    customerId INT,
    numTicket VARCHAR(4) NOT NULL,
-   startDate DATETIME,
+   startDate DATETIME NOT NULL,
    endDate DATETIME,
    ticketType ENUM ('oneTime', 'monthly', 'yearly'),
-   PRIMARY KEY(ticketId),
+   PRIMARY KEY(rideid),
    FOREIGN KEY(scooterId) REFERENCES Scooters(scooterId)
 					ON DELETE SET NULL,
    FOREIGN KEY(customerId) REFERENCES Customers(customerId)
 					ON DELETE SET NULL,
    UNIQUE (numTicket),
-   CONSTRAINT EndDateBeforeOrSameStartDate CHECK (startdate < enddate)	
+   CONSTRAINT EndDateBeforeOrSameStartDate CHECK (startdate < enddate),
+	CONSTRAINT NotRentedLonger3days CHECK (enddate - startdate < 3000000)
 );
 
 CREATE TABLE Reparations(
@@ -123,16 +118,16 @@ CREATE TABLE Reparations(
 	reparationProblem LONGTEXT,
 	PRIMARY KEY(reparationId),
 	FOREIGN KEY(scooterId) REFERENCES Scooters(scooterId)
-					ON DELETE CASCADE,
+					ON DELETE SET NULL,
 	FOREIGN KEY(warehouseId) REFERENCES Warehouses (warehouseId)
-						ON DELETE CASCADE,
+						ON DELETE SET NULL,
 	CONSTRAINT EndDateBeforeOrSameStartDate CHECK (startdate < enddate )	
 );
 
 CREATE TABLE adCampaigns(
 	adCampaignId INT NOT NULL AUTO_INCREMENT,
 	salesManagerId INT,
-	description LONGTEXT,
+	description LONGTEXT NOT NULL,
 	adDate DATE,
 	moneySpent DOUBLE,
 	profit DOUBLE, 
